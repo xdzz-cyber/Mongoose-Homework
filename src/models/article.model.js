e1671b9c8258225ebf621e6f1d8da6ed9f63f7b4
Bohdan Habruk <55808715+xdzz-cyber@users.noreply.github.com>
2023-05-14 17:19:42 +0300

    add models

---
+++ src/models/article.model.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+
+const articleSchema = new mongoose.Schema({
+    title: {type: String, minlength: 5, maxlength: 400, required: true, trim: true},
+    subtitle: {type: String, minlength: 5, maxlength: 400, required: false, trim: true},
+    description: {type: String, minlength: 5, maxlength: 5000, required: true},
+    owner: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    category: {type: String, enum: ['sport', 'games', 'history'], required: true},
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date}
+});
+
+articleSchema.pre('save', function (next) {
+    this.updatedAt = Date.now();
+
+    next();
+});
+
+const Article = mongoose.model('Article', articleSchema);
+
+export default Article;
---
+++ src/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+
+const userSchema = new mongoose.Schema({
+    firstName: {type: String, minlength: 4, maxlength: 50, required: true, trim: true},
+    lastName: {type: String, minlength: 3, maxlength: 60, required: true, trim: true},
+    fullName: {type: String},
+    email: {
+        type: String, required: true, match:
+            [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+        lowercase: true
+    },
+    role: {type: String, enum: ["admin", "writer", "guest"]},
+    age: {type: Number, min: 1, max: 99},
+    numberOfArticles: {type: Number, default: 0},
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date}
+});
+
+userSchema.pre('save', function (next) {
+    this.fullName = `${this.firstName} ${this.lastName}`;
+
+    if (this.age === 0) {
+        this.age = 1;
+    }
+
+    this.updatedAt = Date.now();
+
+    next();
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
